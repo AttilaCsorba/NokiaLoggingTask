93c81c3695a37820603a16b4a4f4452c04deb875
ateszkamatesz <68356776+ateszkamatesz@users.noreply.github.com>
2020-07-16 03:28:00 +0200

    Update

--- Logger2/loggingService.h
+++ Logger2/loggingService.h
@@ -29,33 +29,14 @@ namespace attrs = boost::log::attributes;
 const string g_format = "[%TimeStamp%] ID: [%ID%] Severity: (%Severity%) Message: %Message%";
 class logger
 {
+private:
+    string console_level = "WARNING";
+
 public:
-    void init(string file) {
-        boost::log::add_common_attributes();
-        boost::shared_ptr< logging::core > core = logging::core::get();
-        core->add_global_attribute("TimeStamp", boost::log::attributes::local_clock());
-        core->add_global_attribute("ID", attrs::counter< unsigned int >(1));
-        boost::log::register_simple_formatter_factory< loglevel, char >("Severity");
-        logging::add_console_log(std::cout, boost::log::keywords::format = g_format);
+    void init(string file);
 
-        /*logging::add_file_log
-        (
-            keywords::file_name = file + ".txt",
-            keywords::target_file_name = file + "%N.txt",
-            keywords::rotation_size = 10 * 1024 * 1024,
-            keywords::max_files = 10,
-            keywords::format = g_format
-        );*/
-    }
+    logger(string file);
 
-	logger(string file) {
-        init(file);
-	}
-
-    void log(string message, loglevel level)
-    {
-        src::severity_logger<loglevel> slg;
-        BOOST_LOG_SEV(slg,level) << message;
-    }
+    void log(string message, loglevel level);
 };
 
