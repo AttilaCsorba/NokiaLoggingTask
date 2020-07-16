a2c06cd283f0e5fad82890bf1bc491ff15f2a844
ateszkamatesz <68356776+ateszkamatesz@users.noreply.github.com>
2020-07-16 04:02:53 +0200

    Updaaate

---
+++ Logger2/logger_service.cpp
@@ -0,0 +1,57 @@
+#include "logger_service.h"
+#include "config.h"
+
+logger::logger(string file) {
+    config c;
+    console_level = c.getconsole_level();
+    console_logging = c.getconsole_logging();
+    file_logging = c.getfile_logging();
+    console_format = c.getconsole_format();
+    file_name = c.getfile_name();
+    rotation = c.getrotation();
+    file_full = c.getfile_full();
+    file_max = c.getfile_max();
+    file_format = c.getfile_format();
+    file_level = c.getfile_level();
+    init(file);
+}
+
+void logger::init(string file)
+{
+    boost::log::add_common_attributes();
+    boost::shared_ptr< logging::core > core = logging::core::get();
+    core->add_global_attribute("TimeStamp", boost::log::attributes::local_clock());
+    core->add_global_attribute("ID", attrs::counter< unsigned int >(1));
+    boost::log::register_simple_formatter_factory< loglevel, char >("Severity");
+    if(console_logging)
+        logging::add_console_log(std::cout, boost::log::keywords::format = console_format);
+
+    if (file_logging) {
+        /*logging::add_file_log
+        (
+            keywords::file_name = file + ".txt",
+            keywords::target_file_name = file + "%N.txt",
+            keywords::rotation_size = 10 * 1024 * 1024,
+            keywords::max_files = 10,
+            keywords::format = ;
+        );*/
+    }
+}
+
+void logger::log(string message, loglevel level)
+{
+    loglevel current_level1;
+    if (console_level == "DEBUG")
+        current_level1 = loglevel::DEBUG;
+    else if (console_level == "INFO")
+        current_level1 = loglevel::INFO;
+    else if (console_level == "WARNING")
+        current_level1 = loglevel::WARNING;
+    else if (console_level == "ERROR")
+        current_level1 = loglevel::ERROR;
+
+    if (level >= current_level1) {
+        src::severity_logger<loglevel> slg;
+       BOOST_LOG_SEV(slg, level) << message;
+    }
+}
--- Logger2/main.cpp
+++ Logger2/main.cpp
@@ -1,10 +1,10 @@
-#include "loggingService.h"
+#include "logger_service.h"
 
 
 int main(int, char* [])
 {
     logger log1("log");
-    log1.log("Hello",loglevel::ERROR);
-
+    log1.log("Hello",loglevel::WARNING);
+    
     return 0;
 }
