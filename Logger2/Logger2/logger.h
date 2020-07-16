4170921fe7060dab29a21095cab991acf4d18e9c
ateszkamatesz <68356776+ateszkamatesz@users.noreply.github.com>
2020-07-16 03:00:18 +0200

    Update

--- Logger2/Logger2/logger.h
+++ Logger2/Logger2/logger.h
@@ -10,6 +10,13 @@
 #include <boost/log/utility/setup/console.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/attributes/scoped_attribute.hpp>
+#include <boost/log/attributes/attribute.hpp>
+#include <boost/log/attributes/attribute_cast.hpp>
+#include <boost/log/attributes/attribute_value.hpp>
+#include <boost/log/attributes/constant.hpp>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp> 
+#include <boost/uuid/uuid_io.hpp>
 #include "loglevel.h"
 
 using namespace std;
@@ -18,24 +25,27 @@ namespace logging = boost::log;
 namespace src = boost::log::sources;
 namespace sinks = boost::log::sinks;
 namespace keywords = boost::log::keywords;
-const string g_format = "[%Severity%]: %Message%";
-
-
+namespace attrs = boost::log::attributes;
+const string g_format = "[%TimeStamp%] ID: [%ID%] Severity: (%Severity%) Message: %Message%";
 class logger
 {
 public:
     void init(string file) {
+        boost::log::add_common_attributes();
+        boost::shared_ptr< logging::core > core = logging::core::get();
+        core->add_global_attribute("TimeStamp", boost::log::attributes::local_clock());
+        core->add_global_attribute("ID", attrs::counter< unsigned int >(1));
         boost::log::register_simple_formatter_factory< loglevel, char >("Severity");
         logging::add_console_log(std::cout, boost::log::keywords::format = g_format);
 
-        logging::add_file_log
+        /*logging::add_file_log
         (
             keywords::file_name = file + ".txt",
             keywords::target_file_name = file + "%N.txt",
             keywords::rotation_size = 10 * 1024 * 1024,
             keywords::max_files = 10,
             keywords::format = g_format
-        );
+        );*/
     }
 
 	logger(string file) {
